1e0d352a9febe6710d0867282a3a4e46502ab948
juankhusuma <juan.d.khusuma@gmail.com>
2025-04-05 21:22:08 +0700

    modified router base path

--- chronos/src/app/main.py
+++ chronos/src/app/main.py
@@ -26,7 +26,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-app.include_router(chat_router, prefix="/api/v1", tags=["chat"])
+app.include_router(chat_router, tags=["chat"])
 
 @app.get("/")
 def health_check():
